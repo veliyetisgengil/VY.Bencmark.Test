dc28f86ecb1d29a7a4856b5c64cc8639105da7b5
veli.yetisgengil <yetisgengil_veli@hotmail.com>
2021-08-30 23:53:34 +0300

    Add project files.

---
+++ VY.Bencmarkdotnet.Test.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace VY.Bencmarkdotnet.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BenchmarkRunner.Run<StringBencmark>();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VY.Bencmarkdotnet.Test.Console/StringBencmark.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VY.Bencmarkdotnet.Test
+{
+    [SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net50,id:"StringID")]
+    [MinColumn,MaxColumn,MeanColumn,MedianColumn]
+    [MemoryDiagnoser]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    public class StringBencmark
+    {
+        int counter = 1000;
+
+        [Benchmark(Baseline = true)]
+        public void NormalStringProcess()
+        {
+            string a = "";
+
+            for (int i = 0; i < counter; i++)
+            {
+                a += i.ToString();
+            }
+        }
+
+        [Benchmark]
+        public void BuilderStringProcess()
+        {
+            StringBuilder stringBuilder = new StringBuilder(counter);
+
+            for (int i = 0; i < counter; i++)
+            {
+                stringBuilder.Append(i.ToString());
+            }
+        }
+    }
+}
